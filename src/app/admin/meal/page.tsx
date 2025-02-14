99e24e8b4eef91c4e711ec2202650830a85df282
unknown <133560614+PhucPhuc1111@users.noreply.github.com>
2025-02-14 10:25:09 +0700

    fix CI

--- src/app/admin/meal/page.tsx
+++ src/app/admin/meal/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import { Table, Space, Button, Input } from "antd"; // Import Space từ antd
 import type { TableColumnsType, TableProps } from "antd";
@@ -10,7 +10,6 @@ import DeleteIngredientModal from "@/components/IngredientModal/DeleteIngredient
 import AddMealPlanModal from "@/components/MealPlanModel/AddMealPlanModal";
 import { Key } from "antd/es/table/interface";
 import DeleteMealPlanModal from "@/components/MealPlanModel/DeleteMealPlanModal";
-import UpdateMealPLanModal from "@/components/MealPlanModel/UpdateMealPLanModal";
 import Link from "next/link";
 import { MealPlan, MealPlanDetail } from "@/types/types";
 
@@ -18,13 +17,12 @@ import { MealPlan, MealPlanDetail } from "@/types/types";
 // import DeleteMealPlanModal from "@/components/MealPlanModel/DeleteMealPlanModal";
 // import AddMealPlanModal from "@/components/MealPlanModel/AddMealPlanModal";
 
-
 // Dữ liệu mock MealPlan và MealPlanDetail với DayNumber: 2
 const dataDetail: MealPlanDetail[] = [
   {
     MealPlanDetailID: 1,
     MealPlanID: 1,
-    MealType:"bữa sáng",
+    MealType: "bữa sáng",
     FoodID: 101,
     FoodName: "Cơm trắng",
     Quantity: 150,
@@ -34,8 +32,8 @@ const dataDetail: MealPlanDetail[] = [
   {
     MealPlanID: 1,
     MealPlanDetailID: 2,
-    MealType:"bữa chiều",
-   FoodID: 102,
+    MealType: "bữa chiều",
+    FoodID: 102,
     FoodName: "Thịt gà",
     Quantity: 100,
     DayNumber: 1,
@@ -44,7 +42,7 @@ const dataDetail: MealPlanDetail[] = [
   {
     MealPlanDetailID: 3,
     MealPlanID: 2,
-    MealType:"bữa chiều",
+    MealType: "bữa chiều",
     FoodID: 106,
     FoodName: "Khoai tây",
     Quantity: 200,
@@ -54,7 +52,7 @@ const dataDetail: MealPlanDetail[] = [
   {
     MealPlanDetailID: 4,
     MealPlanID: 2,
-    MealType:"bữa chiều",
+    MealType: "bữa chiều",
     FoodID: 107,
     FoodName: "Thịt bò",
     Quantity: 150,
@@ -63,37 +61,33 @@ const dataDetail: MealPlanDetail[] = [
   },
 ];
 const data: MealPlan[] = [
-    {
-      MealPlanID: 1,
-      UserID: 1,
-      PlanName: "Kế hoạch ăn kiêng giảm cân",
-      HealthGoal: "Giảm cân",
-      Duration: 30,
-      Status: "Active",
-      CreatedBy: "Admin",
-      CreatedAt: "2023-01-01T10:00:00",
-      UpdatedBy: "Admin",
-      UpdatedAt: "2023-01-02T10:00:00",
-      MealPlanDetails: [
-       1,2,3,4
-      ],
-    },
-    {
-      MealPlanID: 2,
-      UserID: 2,
-      PlanName: "Kế hoạch ăn tăng cơ",
-      HealthGoal: "Tăng cơ",
-      Duration: 45,
-      Status: "Active",
-      CreatedBy: "Admin",
-      CreatedAt: "2023-01-01T10:00:00",
-      UpdatedBy: "Admin",
-      UpdatedAt: "2023-01-02T10:00:00",
-      MealPlanDetails: [
-      1,2,3,4
-      ],
-    },
-  ];
+  {
+    MealPlanID: 1,
+    UserID: 1,
+    PlanName: "Kế hoạch ăn kiêng giảm cân",
+    HealthGoal: "Giảm cân",
+    Duration: 30,
+    Status: "Active",
+    CreatedBy: "Admin",
+    CreatedAt: "2023-01-01T10:00:00",
+    UpdatedBy: "Admin",
+    UpdatedAt: "2023-01-02T10:00:00",
+    MealPlanDetails: [1, 2, 3, 4],
+  },
+  {
+    MealPlanID: 2,
+    UserID: 2,
+    PlanName: "Kế hoạch ăn tăng cơ",
+    HealthGoal: "Tăng cơ",
+    Duration: 45,
+    Status: "Active",
+    CreatedBy: "Admin",
+    CreatedAt: "2023-01-01T10:00:00",
+    UpdatedBy: "Admin",
+    UpdatedAt: "2023-01-02T10:00:00",
+    MealPlanDetails: [1, 2, 3, 4],
+  },
+];
 const columns: TableColumnsType<MealPlan> = [
   {
     title: "Id",
@@ -111,13 +105,14 @@ const columns: TableColumnsType<MealPlan> = [
     filters: [
       { text: "Giảm cân", value: "Giảm cân" },
       { text: "Tăng cân", value: "Tăng cân" },
-      { text: "Giữ cân", value: "Giữ cân" },  
-      { text: "Tăng cơ - giảm mỡ", value: "Tăng cơ - giảm mỡ" },  
-     
+      { text: "Giữ cân", value: "Giữ cân" },
+      { text: "Tăng cơ - giảm mỡ", value: "Tăng cơ - giảm mỡ" },
     ],
     onFilter: (value: string | boolean | Key, record: MealPlan) => {
       if (typeof record.HealthGoal === "string" && typeof value === "string") {
-        return record.HealthGoal.toLowerCase().trim().includes(value.toLowerCase().trim());
+        return record.HealthGoal.toLowerCase()
+          .trim()
+          .includes(value.toLowerCase().trim());
       }
       return false;
     },
@@ -127,7 +122,6 @@ const columns: TableColumnsType<MealPlan> = [
     title: "Thời gian",
     dataIndex: "Duration",
     sorter: (a, b) => a.Duration - b.Duration,
-    
   },
   {
     title: "Status ",
@@ -135,47 +129,49 @@ const columns: TableColumnsType<MealPlan> = [
     filters: [
       { text: "Active", value: "Active" },
       { text: "Inactive", value: "Inactive" },
-     
     ],
     onFilter: (value: string | boolean | Key, record: MealPlan) => {
       if (typeof record.Status === "string" && typeof value === "string") {
-        return record.Status.toLowerCase().trim().includes(value.toLowerCase().trim());
+        return record.Status.toLowerCase()
+          .trim()
+          .includes(value.toLowerCase().trim());
       }
       return false;
     },
     width: "30",
   },
- 
+
   {
     title: "Sửa/Xóa",
     dataIndex: "action",
     render: (_: any, record: MealPlan) => (
       <Space size="middle">
-
-      <Link href={`/admin/meal/${record.MealPlanID}`}> 
-              <Button style={{ backgroundColor: '#2f855a', color: 'white' }}>Chi tiết</Button>    </Link>
-        <DeleteMealPlanModal/>
+        <Link href={`/admin/meal/${record.MealPlanID}`}>
+          <Button style={{ backgroundColor: "#2f855a", color: "white" }}>
+            Chi tiết
+          </Button>{" "}
+        </Link>
+        <DeleteMealPlanModal />
       </Space>
     ),
   },
 ];
 
-
 const onChange: TableProps<MealPlan>["onChange"] = (
   pagination,
   filters,
   sorter,
-  extra
+  extra,
 ) => {
   console.log("params", pagination, filters, sorter, extra);
 };
 
 const MealPage: React.FC = () => {
-  const [searchText, setSearchText] = useState<string>('');
-  
-  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchText(e.target.value);};
+  const [searchText, setSearchText] = useState<string>("");
 
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value);
+  };
 
   const filteredData = data.filter((item) => {
     return item.PlanName.toLowerCase().includes(searchText.toLowerCase());
@@ -183,25 +179,29 @@ const MealPage: React.FC = () => {
   return (
     <DefaultLayout>
       <div className="">
-        <div className="flex justify-between" >
-        <div className="mb-2">
-          Tổng cộng: {data.length}
-        </div>
-         <Input
-          placeholder="Tìm kiếm thực phẩm"
-          value={searchText}
-          onChange={handleSearch} 
-          style={{ marginBottom: 20, width: 300 }}
-        />
+        <div className="flex justify-between">
+          <div className="mb-2">Tổng cộng: {data.length}</div>
+          <Input
+            placeholder="Tìm kiếm thực phẩm"
+            value={searchText}
+            onChange={handleSearch}
+            style={{ marginBottom: 20, width: 300 }}
+          />
 
-        <div className="flex space-x-3 mb-2">
-        <Link href={"/admin/meal/create-meal"}> 
-        <Button style={{ backgroundColor: '#2f855a', color: 'white' }}>Thêm kế hoạch</Button>    </Link>
-          
+          <div className="mb-2 flex space-x-3">
+            <Link href={"/admin/meal/create-meal"}>
+              <Button style={{ backgroundColor: "#2f855a", color: "white" }}>
+                Thêm kế hoạch
+              </Button>{" "}
+            </Link>
+          </div>
         </div>
-        </div>
-       
-        <Table<MealPlan> columns={columns} dataSource={filteredData} onChange={onChange} />
+
+        <Table<MealPlan>
+          columns={columns}
+          dataSource={filteredData}
+          onChange={onChange}
+        />
       </div>
     </DefaultLayout>
   );
