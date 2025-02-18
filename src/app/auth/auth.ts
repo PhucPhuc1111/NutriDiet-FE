bbdd66011c4dcfbde2fae080da2b2056b448adad
unknown <133560614+PhucPhuc1111@users.noreply.github.com>
2025-02-18 21:42:59 +0700

    fix SigninFormSchema

---
+++ src/app/auth/auth.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const SigninFormSchema = z.object({
+  email: z.string().email({ message: "Please enter a valid email." }).trim(),
+  password: z.string().min(6, { message: "Password must be at least 6 characters." }),
+});
+
+export type SigninFormData = z.infer<typeof SigninFormSchema>;
