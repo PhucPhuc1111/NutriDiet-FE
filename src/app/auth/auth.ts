9c5b03078450cceab3f55f7b1610d8dc7723314d
unknown <133560614+PhucPhuc1111@users.noreply.github.com>
2025-02-19 12:43:28 +0700

    test

--- src/app/auth/auth.ts
+++ src/app/auth/auth.ts
@@ -1,5 +1,4 @@
 import { z } from "zod";
-//test
 export const SigninFormSchema = z.object({
   email: z.string().email({ message: "Please enter a valid email." }).trim(),
   password: z.string().min(6, { message: "Password must be at least 6 characters." }),
