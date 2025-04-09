4795be54118e5984b942877b892e7372dd6780f6
PhucPhuc1111 <phuctoi8930@gmail.com>
2025-04-09 07:41:30 +0700

    deploy

--- src/components/Header/DropdownUser.tsx
+++ src/components/Header/DropdownUser.tsx
@@ -16,29 +16,45 @@ const DropdownUser = ({
   return (
     <div>
       {user ? (
-        <ClickOutside onClick={() => setDropdownOpen(false)} className="relative">
-          <button onClick={() => setDropdownOpen(!dropdownOpen)} className="flex items-center gap-4">
+        <ClickOutside
+          onClick={() => setDropdownOpen(false)}
+          className="relative"
+        >
+          <button
+            onClick={() => setDropdownOpen(!dropdownOpen)}
+            className="flex items-center gap-4"
+          >
             <span className="hidden text-right lg:block">
-              <span className="block text-sm font-medium text-black">{user.name}</span>
+              <span className="block text-sm font-medium text-black">
+                {user.name}
+              </span>
               <span className="block text-xs">{user.role}</span>
             </span>
             <span className="h-12 w-12 rounded-full">
-              <Image src="/images/user/user-01.png" width={112} height={112} alt="User" />
+              <Image
+                src="/images/user/user-01.png"
+                width={112}
+                height={112}
+                alt="User"
+              />
             </span>
           </button>
 
           {dropdownOpen && (
-            <div className="absolute right-0 mt-4 w-48 bg-white shadow-lg rounded-lg">
+            <div className="absolute right-0 mt-4 w-48 rounded-lg bg-white shadow-lg">
               <ul>
                 <li>
-                  <Link href="/admin/dashboard" className="block px-4 py-2 hover:bg-gray-100">
+                  <Link
+                    href="/admin/dashboard"
+                    className="block px-4 py-2 hover:bg-gray-100"
+                  >
                     Dashboard
                   </Link>
                 </li>
                 <li>
                   <button
                     onClick={handleLogout}
-                    className="block w-full text-left px-4 py-2 hover:bg-gray-100"
+                    className="block w-full px-4 py-2 text-left hover:bg-gray-100"
                   >
                     Đăng xuất
                   </button>
@@ -49,7 +65,9 @@ const DropdownUser = ({
         </ClickOutside>
       ) : (
         <Link href="/auth/signin">
-          <button className="px-4 py-2 bg-green-800 text-white rounded">Quản trị viên đăng nhập</button>
+          <button className="rounded bg-green-800 px-4 py-2 text-white">
+            Quản trị viên đăng nhập
+          </button>
         </Link>
       )}
     </div>
